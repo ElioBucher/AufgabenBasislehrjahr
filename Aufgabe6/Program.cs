18d74ce8ac91f9a75b3378f23305c128b2b9eb82
eliobucher <elio.bucher10@gmail.com>
2025-09-29 09:15:21 +0200

    Neue Aufgaben

---
+++ Aufgabe6/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Aufgabe6;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("-----------");
+        Console.WriteLine("Kleines 1x1");
+        Console.WriteLine("-----------");
+        
+        for (int i = 1; i <= 100; i++)
+        {
+            Console.Write(i + "\t");
+            if (i % 10 == 0)
+            {
+                Console.WriteLine(" ");
+            }
+        }
+    }
+}
---
+++ Aufgabe7/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Aufgabe7;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Zahlen zwischen 1 und 30, die durch 5 und/oder 3 ohne Rest teilbar sind:");
+
+        bool first = true;
+        for (int i = 1; i <= 30; i++)
+        {
+            if (i % 3 == 0 || i % 5 == 0)
+            {
+                if (!first)
+                {
+                    Console.Write(", ");
+                }
+
+                Console.Write(i);
+                first = false;
+            }
+        }
+    }
+}
